941985a0e3b7f7e417363d50f8574678d60c3d22
Daniel Avellaneda <davellanedam@gmail.com>
2017-08-31 01:46:40 -0500

    users model

---
+++ models/Users.php
@@ -0,0 +1,190 @@
+<?php
+
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Email as EmailValidator;
+
+class Users extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $username;
+
+    /**
+     *
+     * @var string
+     */
+    public $password;
+
+    /**
+     *
+     * @var string
+     */
+    public $firstname;
+
+    /**
+     *
+     * @var string
+     */
+    public $lastname;
+
+    /**
+     *
+     * @var string
+     */
+    public $level;
+
+    /**
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     *
+     * @var string
+     */
+    public $phone;
+
+    /**
+     *
+     * @var string
+     */
+    public $mobile;
+
+    /**
+     *
+     * @var string
+     */
+    public $address;
+
+    /**
+     *
+     * @var string
+     */
+    public $country;
+
+    /**
+     *
+     * @var string
+     */
+    public $city;
+
+    /**
+     *
+     * @var string
+     */
+    public $birthday;
+
+    /**
+     *
+     * @var integer
+     */
+    public $authorised;
+
+    /**
+     *
+     * @var string
+     */
+    public $block_expires;
+
+    /**
+     *
+     * @var integer
+     */
+    public $login_attempts;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setConnectionService('db');
+    }
+
+    /**
+     * Validations and business logic
+     *
+     * @return boolean
+     */
+    public function validation()
+    {
+        $validator = new Validation();
+
+        $validator->add(
+            'email',
+            new EmailValidator()
+        );
+
+        return $this->validate($validator);
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'users';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Independent Column Mapping.
+     * Keys are the real names in the table and the values their names in the application
+     *
+     * @return array
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id',
+            'username' => 'username',
+            'password' => 'password',
+            'firstname' => 'firstname',
+            'lastname' => 'lastname',
+            'level' => 'level',
+            'email' => 'email',
+            'phone' => 'phone',
+            'mobile' => 'mobile',
+            'address' => 'address',
+            'country' => 'country',
+            'city' => 'city',
+            'birthday' => 'birthday',
+            'authorised' => 'authorised',
+            'block_expires' => 'block_expires',
+            'login_attempts' => 'login_attempts'
+        );
+    }
+
+}
