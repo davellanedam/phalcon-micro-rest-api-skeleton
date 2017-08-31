45b280decede52eecc9a10cb6926adba44d39e9c
Daniel Avellaneda <davellanedam@gmail.com>
2017-08-30 23:32:12 -0500

    deploy staging

---
+++ deploy-staging.sh
@@ -0,0 +1,4 @@
+PROJECT=myproject
+USER=server_username
+URL=my_staging_server_url
+read -p "`echo $'\n*****************************'` `echo $'\n******** API STAGING ********'` `echo $'\n*****************************'` `echo $'\nPress Return to Deploy...'`"; rsync -avzhe ssh --exclude-from=$HOME/$PROJECT/exclude-staging.txt $HOME/$PROJECT/ $USER@$URL:/home/$USER/$URL; read -p "Press Return to Close..."
