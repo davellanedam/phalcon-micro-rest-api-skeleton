4354d70b415dfe86e7cd0a0896b219a85fadf469
Daniel Avellaneda <davellanedam@gmail.com>
2017-08-31 00:09:19 -0500

    users access model

---
+++ models/UsersAccess.php
@@ -0,0 +1,107 @@
+<?php
+
+class UsersAccess extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $username;
+
+    /**
+     *
+     * @var string
+     */
+    public $ip;
+
+    /**
+     *
+     * @var string
+     */
+    public $domain;
+
+    /**
+     *
+     * @var string
+     */
+    public $country;
+
+    /**
+     *
+     * @var string
+     */
+    public $browser;
+
+    /**
+     *
+     * @var string
+     */
+    public $date;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setConnectionService('db');
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'users_access';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return UsersAccess[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return UsersAccess
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Independent Column Mapping.
+     * Keys are the real names in the table and the values their names in the application
+     *
+     * @return array
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id',
+            'username' => 'username',
+            'ip' => 'ip',
+            'domain' => 'domain',
+            'country' => 'country',
+            'browser' => 'browser',
+            'date' => 'date'
+        );
+    }
+
+}
