d4de3bbc53fa7f3f12fd67c5e17aa5a8229db309
Daniel Avellaneda <davellanedam@gmail.com>
2017-08-30 23:58:10 -0500

    loader

---
+++ config/loader.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Registering an autoloader
+ */
+$loader = new \Phalcon\Loader();
+
+$loader->registerDirs(
+    array(
+        $config->application->controllersDir,
+        $config->application->middlewaresDir,
+        $config->application->modelsDir,
+        $config->application->libraryDir
+    )
+)->register();
