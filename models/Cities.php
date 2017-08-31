031a08a828626a58b8b35f1141c121c0d755da56
Daniel Avellaneda <davellanedam@gmail.com>
2017-08-31 00:00:31 -0500

    cities model

---
+++ models/Cities.php
@@ -0,0 +1,79 @@
+<?php
+
+class Cities extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $country;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setConnectionService('db');
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'city';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return City[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return City
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Independent Column Mapping.
+     * Keys are the real names in the table and the values their names in the application
+     *
+     * @return array
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id',
+            'name' => 'name',
+            'country' => 'country'
+        );
+    }
+
+}
